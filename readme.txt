22a683947023bb0067ad14e40f46ec84cabe5027
adityasubawa <adityasubawa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-10 12:31:57 +0000

    First Upload
    git-svn-id: https://plugins.svn.wordpress.org/my-quick-search/trunk@665886 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== My Quick Search ===
+
+Contributors: adityasubawa
+Donate link: http://www.adityasubawa.com
+Tags: search, quick search, custom search, simple search, easy search
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 1.0
+
+Display custom quick search form as a stylish search form widgets in WordPress. very simple and easy to install.  
+
+== Description ==
+
+My Quick Search is a very simple wordpress plugins to display custom quick search form as a stylish search form widgets. No configuration at all, very simple and easy to install.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `my-quick-search.zip` to the `/wp-content/plugins/` directory. You can do this using 'Upload' functionality provided in plugins section of your wordpress dashboard
+2. Activate the plugin through the `Plugins` menu in WordPress
+3. Go to `Appearances` >> `My Quick Search` and Drag `My Quick Search` in to your WordPress sidebar.
+4. Save
+5. You are done. 
+
+== Screenshots ==
+
+1. Display Example
+
+== Frequently Asked Questions ==
+
+= How to editing CSS of `My Quick Search` widgets? =
+
+you must open the file `wp-quicksearch.php` plugins on the `wp-content/plugins/my-quick-search/` and look for the following CSS listing after `echo" <style type="text/css">` and edit as you wish. (You must understanding basic CSS code) 
+
+== Changelog ==
+
+= 1.0 =
+
+Plug-in is now compatible upto wordpress version 3.5.1
+
+== Credits ==
+
+[Aditya Subawa](http://www.adityasubawa.com/) - This plugin was created by Aditya Subawa. If you like this widget and would like to donate to help support new versions of this widget you can do so at the <a href="http://www.adityasubawa.com">support center</a> website.
+
+== Contact ==
+
+If you need help, support or would just like to provide your comments and suggestions you may visit us online at <a href="http://www.adityasubawa.com">my personal website</a> or <a href="http://balimechanicweb.net">web design</a> support center.
---
+++ wp-quicksearch.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: My Quick Search
+Plugin URI: http://www.adityasubawa.com/wordpress-plugins.php
+Description: Display quick custom search form as a stylish search form widgets in WordPress.
+Version: 1.0
+Author: Aditya Subawa
+Author URI: http://www.adityasubawa.com
+*/
+class wp_adityasearch extends WP_Widget{
+    
+    function __construct(){
+     $params=array(
+            'description' => 'Display Simple quick custom search form', //deskripsi  dari plugin  yang di tampilkan
+            'name' => 'My Quick Search'  //title dari plugin
+        );
+        
+        parent::__construct('wp_adityasearch', '', $params); 
+    }
+    
+    public function form($instance){
+       ?>
+<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<p><label for="<?php echo $this->get_field_id('author_credit'); ?>"><?php _e('Give credit to plugin author?'); ?><input type="checkbox" class="checkbox" <?php checked( $instance['author_credit'], 'on' ); ?> id="<?php echo $this->get_field_id('author_credit'); ?>" name="<?php echo $this->get_field_name('author_credit'); ?>" /></label></p>
+<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ZMEZEYTRBZP5N&lc=ID&item_name=Aditya%20Subawa&item_number=426267&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted" target="_blank"><img src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" alt="<?_e('Donate')?>" /></a></p>
+
+	   <?php
+    }
+    
+    public function widget($args, $instance){
+      extract($args, EXTR_SKIP);
+    $authorcredit = isset($instance['author_credit']) ? $instance['author_credit'] : false ; // give plugin author credit
+    echo $before_widget;
+    $title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+	 ?>
+	 <form class="quick_search" method="post" id="adityasearch" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<p>
+<input type="text" name="s" id="s" value="Quick Search" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;" />
+</p>
+</form>
+<?php
+echo "<style type=\"text/css\">
+#adityasearch {
+text-align: center;
+}
+
+#adityasearch input[type=text] {
+-webkit-border-radius: 20px;
+-moz-border-radius: 20px;
+border-radius: 20px;
+border: 1px solid #bbb;
+height: 26px;
+width: 100%;
+color: #ccc;
+-webkit-box-shadow: inset 0 2px 2px #ccc, 0 1px 0 #fff;
+-moz-box-shadow: inset 0 2px 2px #ccc, 0 1px 0 #fff;
+box-shadow: inset 0 2px 2px #ccc, 0 1px 0 #fff;
+text-indent: 30px;
+background: #fff url(http://www.adityasubawa.com/images/icn_search.png) no-repeat;
+background-position: 10px 6px;
+}
+
+#adityasearch input[type=text]:focus {
+outline: none;
+color: #666666;
+border: 1px solid #77BACE;
+-webkit-box-shadow: inset 0 2px 2px #ccc, 0 0 10px #ADDCE6;
+-moz-box-shadow: inset 0 2px 2px #ccc, 0 0 10px #ADDCE6;
+box-shadow: inset 0 2px 2px #ccc, 0 0 10px #ADDCE6;
+}
+</style>";
+
+	 if ($authorcredit) { ?>
+			<p style="font-size:10px;">
+				Plugins by <a href="http://www.adityasubawa.com" title="Bali Web Design">Bali Web Design</a>
+			</p>
+			<?php }
+	echo $after_widget;
+  }
+}
+add_action('widgets_init', 'register_wp_adityasearch');
+function register_wp_adityasearch(){
+    register_widget('wp_adityasearch');
+}
+?>
